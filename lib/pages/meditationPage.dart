4eaa96045d7e083f4c13abeac9c25c039c191df9
Mert Palaoğlu <mertpalaoglu1@gmail.com>
2024-10-12 21:52:32 +0300

    Add audioplayer and Setup audioplayer

--- lib/pages/meditationPage.dart
+++ lib/pages/meditationPage.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
 class meditationPage extends StatefulWidget {
@@ -8,7 +9,23 @@ class meditationPage extends StatefulWidget {
 }
 
 class _meditationPageState extends State<meditationPage> {
+  late AudioPlayer player = AudioPlayer();
   @override
+  void initState() {
+    super.initState();
+    player = AudioPlayer(); //created an audio instance
+    player.setReleaseMode(ReleaseMode.stop);
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      await player.setSource(AssetSource('')); //asset eklenecek.
+    });
+  }
+
+  void dispose() {
+    // iş bittikten sonra player dispose edilir.
+    player.dispose();
+    super.dispose();
+  }
+
   Widget build(BuildContext context) {
     return Column(
       children: [
